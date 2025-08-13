9083e3035b085419a9bb13fe40e9ee4d10946bc8
Vinicius Celes <vinius.celes73@outlook.com>
2025-08-12 21:12:33 -0300

    Atualizando sistema de cadastro de filmes

--- 17 - Cadastro de filmes/17 - Cadastro de filmes.py
+++ 17 - Cadastro de filmes/17 - Cadastro de filmes.py
@@ -12,59 +12,49 @@
 # gênero
 # Os filmes devem ser armazenados em uma lista.
 
-#PENDENCIAS:
-# Salvar os dados em um arquivo .txt (nível intermediário).
-# Substituir códigos repetidos por funções
-    # def exibir filmes
-    # def cadastrar filmes
-    # def buscar por generos
-    # def alterar filmes 
-    # def excluir filmes
 
-opcoes = ['Cadastrar novo filme', 'Listar todos os filmes', 'Buscar filmes por gênero','Alterar filme', 'Excluir filme', 'Sair']
-filmes = []
-generos = set()
+import json
 
+def importar_lista():
+    importados = []
+    try:
+        with open("lista_filmes.json", "r", encoding="utf8") as lista_filmes:
+            importados = json.load(lista_filmes)
+        return importados
+    except:
+        print("criando arquivo...")
+        salvar_lista(importados)
+        return importados
 
-while True:
-    print('\nOlá, esse é o sistema de cadastro de filmes') #Introdução
-    for i, opcao in enumerate(opcoes, 1): #Mostra opcoes
-        print(f'{i}) {opcao}')
-    
-    escolha = input('Escolha uma das opções: ') #Pede entrada de uma das opcoes
-    print()
-    
-    try: #Valida a escolha
-        escolha = int(escolha) 
-        if escolha > len(opcoes) or escolha < 1:
-            print(f'\nEscolha uma opção de 1 a {len(opcoes)}\n')
+def salvar_lista(lista):
+    with open("lista_filmes.json", 'w', encoding="utf8") as lista_filmes:
+        json.dump(lista, lista_filmes, indent=2)
 
-    except ValueError:
-        print(f'Uma opção de 1 a {len(opcoes)}. Letras não são uma opção válida\n')
-
-    if escolha == 1: #Cadatrar filme
-      titulo = input('Digite o titulo do filme: ')  
-      ano = input('Digite o ano do filme: ')
+def cadastrar(lista):
+    while True:
+        titulo = input('Digite o titulo do filme: ')  
+        ano = input('Digite o ano do filme: ')
       
-      if not ano.isdigit():
-          print('Digite apenas numeros para o ano')
-          continue
-      
-      genero = input('Digite o genero do filme: ')
-      filme_novo = {'titulo' : titulo, 'ano' : ano, 'genero' : genero}
-      filmes.append(filme_novo)
+        if not ano.isdigit():
+            print('Digite apenas numeros para o ano')
+            continue
+        genero = input('Digite o genero do filme: ')
+        filme_novo = {'titulo' : titulo, 'ano' : ano, 'genero' : genero}
+        lista.append(filme_novo)
+        return filme_novo
 
-    elif escolha == 2: #Mostrar filmes cadastrados
-        for filme in filmes:
-            print(f'Titulo: {filme['titulo']}')
-            print(f'Ano: {filme['ano']}')
-            print(f'Genero: {filme['genero']}\n')
+def exibir_filmes(lista):
+    for filme in lista:
+        print(f'Titulo: {filme['titulo']}')
+        print(f'Ano: {filme['ano']}')
+        print(f'Genero: {filme['genero']}\n')
 
-    elif escolha == 3: #Filtrar filmes
+def filtrar_filmes(lista):
+    while True:
         print('Escolha um dos generos:')
-        generos = sorted(set(filme['genero'] for filme in filmes))
+        generos = sorted(set(filme['genero'] for filme in lista))
         for i, genero in enumerate(generos,1):
-            print(f'{i}) {genero}')
+                print(f'{i}) {genero}')
         try:
             genero_escolhido = int(input('escolha um genero: '))
             genero_escolhido -= 1
@@ -75,64 +65,108 @@ while True:
             continue
 
         print(f'\nFilmes do genero {genero_escolhido}:')
-        for filme in filmes:
+        for filme in lista:
             if filme['genero'] == genero_escolhido:
                 print(f" - {filme['titulo']} ({filme['ano']})")
+        break
 
-    elif escolha == 4: #Alterar filme
+def alterar_filmes(lista):
+    while True:
         print('Filmes cadastrados:')
-        for i, filme in enumerate(filmes, 1):
+        for i, filme in enumerate(lista, 1):
             print(f'{i}) Titulo: {filme['titulo']}')
             print(f'  Ano: {filme['ano']}')
             print(f'  Genero: {filme['genero']}')
             print()
         try:
             alterar = int(input('Escolha um dos filmes: '))
-            if alterar < 1 or alterar > len(filmes):
-                print('Digite um numero valido')
+            if alterar < 1 or alterar > len(lista):
+                print(f'Digite um numero valido de 1 a {len(lista)}')
                 continue
             alterar -= 1
-            print(f'1) Titulo: {filmes[alterar]['titulo']}')
-            print(f'2) Ano: {filmes[alterar]['ano']}')
-            print(f'3) Genero: {filmes[alterar]['genero']}')
+            print(f'1) Titulo: {lista[alterar]['titulo']}')
+            print(f'2) Ano: {lista[alterar]['ano']}')
+            print(f'3) Genero: {lista[alterar]['genero']}')
             alterar_atributo = int(input('Escolha o que deseja alterar: '))
-            
+                
             if alterar_atributo == 1:
                 alt_titulo = input('Digite o titulo: ')
-                filmes[alterar]['titulo'] = alt_titulo
+                lista[alterar]['titulo'] = alt_titulo
             if alterar_atributo == 2:
                 alt_ano = input('Digite o ano: ')
                 if not alt_ano.isdigit():
                     print('Digite apenas numeros para o ano')
                     continue
-                filmes[alterar]['ano'] = alt_ano
+                lista[alterar]['ano'] = alt_ano
             if alterar_atributo == 3:
                 alt_genero = input('Digite o genero: ')
-                filmes[alterar]['genero'] = alt_genero
+                lista[alterar]['genero'] = alt_genero
             else:
                 print('Escolha uma opção válida')
+            break
 
         except ValueError:
-            print('Digite apenas numeros para selecionar uma das opções')
+                print('Digite apenas numeros para selecionar uma das opções')
 
-    elif escolha == 5: #Excluir filme
+def excluir_filme(lista):
+    while True:
         print('Filmes cadastrados:')
-        for i, filme in enumerate(filmes, 1):
+        for i, filme in enumerate(lista, 1):
             print(f'{i}) Titulo: {filme['titulo']}')
-            print(f'Ano: {filme['ano']}')
-            print(f'Genero: {filme['genero']}')
+            print(f'  Ano: {filme['ano']}')
+            print(f'  Genero: {filme['genero']}')
             print()
         try:
             excluir = int(input('Digite qual filme deseja excluir: '))
-            if excluir < 1 or excluir > len(filmes):
-                print('Digite numeros validos')
+            if excluir < 1 or excluir > len(lista):
+                print(f'Digite numeros validos de 1 a {len(lista)}')
                 continue
             excluir -= 1
-            filmes.pop(excluir)
+            lista.pop(excluir)
             print('filme excluido')
+            break
         except ValueError:
             print('Digite apenas numeros')
 
-    elif escolha == 6: #Sair
-        print('Saindo...')
-        break
+def executar():
+    while True:
+        print('\nOlá, esse é o sistema de cadastro de filmes') #Introdução
+        for i, opcao in enumerate(opcoes, 1): #Mostra opcoes
+            print(f'{i}) {opcao}')
+        
+        escolha = input('Escolha uma das opções: ') #Pede entrada de uma das opcoes
+        print()
+        
+        try: #Valida a escolha
+            escolha = int(escolha) 
+            if escolha > len(opcoes) or escolha < 1:
+                print(f'\nEscolha uma opção de 1 a {len(opcoes)}\n')
+
+        except ValueError:
+            print(f'Uma opção de 1 a {len(opcoes)}. Letras não são uma opção válida\n')
+
+        if escolha == 1: #Cadatrar filme
+            cadastrar(filmes)
+
+        elif escolha == 2: #Mostrar filmes cadastrados
+            exibir_filmes(filmes)
+
+        elif escolha == 3: #Filtrar filmes
+            filtrar_filmes(filmes)
+
+        elif escolha == 4: #Alterar filme
+            alterar_filmes(filmes)
+
+        elif escolha == 5: #Excluir filme
+            excluir_filme(filmes)
+
+        elif escolha == 6: #Sair
+            print('Saindo...')
+            salvar_lista(filmes)
+            break
+
+opcoes = ['Cadastrar novo filme', 'Listar todos os filmes', 'Buscar filmes por gênero','Alterar filme', 'Excluir filme', 'Sair']
+filmes = importar_lista()
+generos = set()
+
+executar()
--- 17 - Cadastro de filmes/README.md
+++ 17 - Cadastro de filmes/README.md
@@ -1,19 +1,19 @@
 # 17 - Cadastro de filmes
 
-Este é um sistema de cadastro e consulta de filmes, feito em Python. Ele funciona como um pequeno programa interativo, que permite ao usuário realizar quatro ações principais: adicionar, visualizar todos os filmes cadastrados, buscar filmes por gênero ou encerrar o programa.
+Este é um sistema de cadastro e consulta de filmes, feito em Python. Ele funciona como um programa interativo, que permite ao usuário realizar seis ações principais: adicionar, visualizar todos os filmes cadastrados, buscar filmes por gênero, alterar filme, excluir filme ou encerrar o programa.
 
-Ao iniciar, o programa exibe uma mensagem de boas-vindas e apresenta um menu com as quatro opções enumeradas:
+Ao iniciar, o programa exibe uma mensagem de boas-vindas e apresenta um menu com as seis opções enumeradas:
 
-Cadastrar novo filme: Se o usuário escolher essa opção, o sistema pergunta pelo título do filme, o ano em que foi lançado e o gênero. Esses dados são armazenados juntos, como se fossem uma ficha de cadastro. O filme é então guardado em uma lista de filmes.
+1) Cadastrar novo filme: Se o usuário escolher essa opção, o sistema pergunta pelo título do filme, o ano em que foi lançado e o gênero. Esses dados são armazenados juntos, como se fossem uma ficha de cadastro. O filme é então guardado em uma lista de filmes.
 
-Listar todos os filmes: Aqui, o programa mostra todos os filmes que já foram cadastrados, um por um, com seus respectivos títulos, anos e gêneros.
+2) Listar todos os filmes: Aqui, o programa mostra todos os filmes que já foram cadastrados, um por um, com seus respectivos títulos, anos e gêneros.
 
-Buscar filmes por gênero: O programa coleta todos os gêneros disponíveis entre os filmes cadastrados e os mostra como opções. O usuário escolhe um deles e o sistema lista apenas os filmes que pertencem ao gênero selecionado.
+3) Buscar filmes por gênero: O programa coleta todos os gêneros disponíveis entre os filmes cadastrados e os mostra como opções. O usuário escolhe um de3les e o sistema lista apenas os filmes que pertencem ao gênero selecionado.
 
-Alterar filme: Lista filmes e pede para escolher uma das opções e em seguida solicita qual atributo do filme o usuário gostaria de alterar e no final altera o atributo após o usuário digitar o valor
+4) Alterar filme: Lista filmes e pede para escolher uma das opções e em seguida solicita qual atributo do filme o usuário gostaria de alterar e no final altera o atributo após o usuário digitar o valor
 
-Excluir filme: Pergunta qual dos filmes enumerados o usuario gostaria de excluir e no fim exclui se for um valor valido
+5) Excluir filme: Pergunta qual dos filmes enumerados o usuario gostaria de excluir e no fim exclui se for um valor valido
 
-Sair: Finaliza o programa.
+6) Sair: Salva os filmes cadastrados em um arquivo .json e Finaliza o programa.
 
 Antes de qualquer ação, o programa verifica se a opção escolhida é válida. Se o usuário digitar algo errado, como uma letra ou um número fora das opções, ele avisa e volta a mostrar o menu. Durante o cadastro de um filme, também verifica se o ano informado contém apenas números.
